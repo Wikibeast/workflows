48377bac973c1fde35a1319676a37c02b4303ede
Wikibeast <david@davidseamans.com>
2015-11-15 20:07:38 -0500

    Added Compass Process

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp'),
 	gutil = require('gulp-util'),
 	coffee = require('gulp-coffee'),
+	browserify = require('gulp-browserify'),
+	compass = require('gulp-compass'),
 	concat = require('gulp-concat');
 
 var coffeeSources = [];			//use an array variable just in case you want to add more later
@@ -10,6 +12,9 @@ var jsSources = [
 	'components/scripts/tagline.js',
 	'components/scripts/template.js'
 ];
+var sassSources = ['components/sass/style.scss'];			//note that this is the 
+														//only necessary sass file because the 
+														//partials are imported here
 
 gulp.task('coffee', function() {
 	gulp.src(coffeeSources)
@@ -21,5 +26,17 @@ gulp.task('coffee', function() {
 gulp.task('js', function() {
 	gulp.src(jsSources)
 		.pipe(concat('script.js'))
+		.pipe(browserify())
 		.pipe(gulp.dest('builds/development/js'))
+});
+
+gulp.task('compass', function() {
+	gulp.src(sassSources)
+		.pipe(compass({									//put all your Sass config here instead of in a file
+			sass: 'components/sass',
+			image: 'builds/development/images',
+			style: 'expanded'
+		}))
+		.on('error', gutil.log)
+		.pipe(gulp.dest('builds/development/css'))
 });
